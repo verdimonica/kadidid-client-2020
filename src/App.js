8c446e863ad3746bd49ab732c16acf447f2f58ad
Monica Verdi <monica.verdi@gmail.com>
2020-12-11 16:50:44 +0100

    add side navbarr and background style

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
+import Navbar from './components/Oldnavbar';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,53 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { withAuth } from './../context/auth-context';
+import { withAuth } from '../context/auth-context';
 
 class Navbar extends Component {
-  render() {
-    // const { user, logout, isLoggedin } = this.props;
-    return (
-      <nav className="navbar">
-        <Link to={'/'} id='home-btn'>
-          <h4>Home</h4>
-        </Link>
-        {this.props.isLoggedIn ? (
-          <>
-            <p>username: {this.props.user && this.props.user.username}</p>
-            <button onClick={this.props.logout}>Logout</button>
-          </>
-        ) : (
-          <>
-            <Link to="/login">
-              <button className="navbar-button">Login</button>{' '}
-            </Link>
-            <br />
-            <Link to="/signup">
-              <button className="navbar-button">Sign Up</button>{' '}
-            </Link>
-          </>
-        )}
-      </nav>
-    );
-  }
+    render() {
+      const {user, logout, isLoggedin} = this.props;
+        return (
+        <div className="side-menu">
+          <div className="left-content">
+            <ul className="action-list">
+              <li className="item">
+              <Link to={'/'}>
+                <span>Home</span>
+              </Link>
+              {this.props.isLoggedIn ?this(
+                <>
+                  <p>username:{this.props.user && this.props.user.username}</p>
+                  <span onClick={this.props.logout}>Sign Out</span>
+                </>
+              ):(
+                <>
+                <li className="item">
+                  <Link to='/signin'>
+                    <span>Sign In</span>
+                  </Link>
+                </li>
+                <li className="item">
+                  <Link to='/signup'>
+                    <span>Sign Up</span>
+                  </Link>
+                </li>
+                <li className="item">
+                  <Link to='/children'>
+                    <span>Children</span>
+                  </Link>
+                </li>
+                <li className="item">
+                  <Link to='/profile'>
+                    <span>My Profile</span>
+                  </Link>
+                </li>
+                </>
+              )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        )
+    }
 }
 
 export default withAuth(Navbar);
